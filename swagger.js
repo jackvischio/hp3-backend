2cd61ec2e342104f2d79acac271e255a57a57e7d
jackvischio <giacomo989jack@gmail.com>
2023-01-01 14:08:46 +0100

    ignore swagger

--- swagger.js
+++
@@ -1,8 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')()
-
-const outputFile = './swagger_output.json'
-const endpointsFiles = ['./routes/*']
-
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
-    require('./app.js')
-})
